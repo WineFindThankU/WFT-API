82b402c0d0a7bfb9b64f1a2c0187f290c472608b
KJH <kjhg5852@naver.com>
2022-01-04 11:33:30 +0900

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# API
+
+## Setting
+```bash
+npm install nodemon -g
+```
+
+## Usage
+### Install
+```bash
+npm install
+```
+
+### Running Dev Server
+```bash
+npm run dev
+```
---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const { PORT } = process.env
+
+import express from 'express'
+const app = express()
+const port = PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('WFT API')
+})
+
+app.listen(port)
