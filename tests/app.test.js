bb211cb208b2a15cceee26f85be370029fbe3bb7
KJH <kjhg5852@naver.com>
2022-01-30 00:22:25 +0900

    Update test code

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
 import app from '../app.js'
 import request from 'supertest'
 import pkg from '@prisma/client'
-import { hash } from 'bcrypt'
+import { hashSync } from 'bcrypt'
 const { PrismaClient } = pkg
 const prisma = new PrismaClient()
 
@@ -9,10 +9,19 @@ describe('App', () => {
   let accessToken
   let refreshToken
 
-  let mainTestID = 'test@test.com'
-  let mainTestPWD = '1234'
+  const mainPWD = '1234'
+  const main = {
+    us_id: 'test@test.com',
+    us_nick: 'test',
+    us_pwd: hashSync(mainPWD, 10),
+  }
 
-  beforeAll(async () => {})
+  const subID = 'test1@test.com'
+  const subPWD = '1234'
+
+  beforeAll(async () => {
+    await prisma.user.create({ data: main })
+  })
 
   afterAll(async () => {
     await prisma.user.deleteMany({})
@@ -27,7 +36,7 @@ describe('App', () => {
     it('회원가입 (Post:/v1/users)', async () => {
       return request(app)
         .post('/v1/users')
-        .send({ id: mainTestID, pwd: mainTestPWD, us_type: 'email' })
+        .send({ id: subID, pwd: subPWD, type: 'email' })
         .expect(201)
     })
   })
@@ -36,7 +45,7 @@ describe('App', () => {
     it('로그인 (Post:/v1/auth/sign)', async () => {
       const response = await request(app)
         .post('/v1/auth/sign')
-        .send({ id: mainTestID, pwd: mainTestPWD })
+        .send({ id: main.us_id, pwd: mainPWD })
         .expect(201)
 
       accessToken = response.body.data.accessToken
@@ -46,18 +55,25 @@ describe('App', () => {
       expect(refreshToken).toBeDefined()
     })
 
-    it('로그인 체크 (Get:/v1/auth/sign)', async () => {
+    it('로그인 체크 (Get:/v1/auth/sign)', () => {
       return request(app)
         .get('/v1/auth/sign')
         .set('Authorization', `Bearer ${accessToken}`)
         .expect(200)
     })
 
-    it('토큰 재발급 (Post:/v1/auth/sign/new)', async () => {
+    it('토큰 재발급 (Post:/v1/auth/sign/new)', () => {
       return request(app)
         .post('/v1/auth/sign/new')
         .set('Authorization', `Bearer ${refreshToken}`)
         .expect(201)
+    })
+
+    it('로그아웃 (Delete:/v1/auth/sign)', () => {
+      return request(app)
+        .delete('/v1/auth/sign')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(200)
     })
   })
 })
