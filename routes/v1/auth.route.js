ceddb1187b138d7592f9cd55701d020046a68ca7
KJH <kjhg5852@naver.com>
2022-07-13 09:54:37 +0900

    docs : Update auth docs

--- routes/v1/auth.route.js
+++ routes/v1/auth.route.js
@@ -14,6 +14,43 @@ import { registType, snsRegistType } from '../../utils/constant.js'
 
 const router = Router()
 
+/**
+ * @openapi
+ * /auth/sign:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: 로그인
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/signInRequest'
+ *           examples:
+ *             "회원가입 (SNS)":
+ *               value:
+ *                 id: test@test.com
+ *                 type: naver
+ *                 sns_id: test
+ *             "회원가입 (EMAIL)":
+ *               value:
+ *                 id: test@test.com
+ *                 type: email
+ *                 pwd: "1234"
+ *     responses:
+ *       201:
+ *         description: 로그인 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/signInSuccessResponse'
+ *             example:
+ *               statusCode: 201
+ *               message: 로그인 성공
+ *               data:
+ *                  accessToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+ *                  refreshToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+ */
 router.post(
   '/sign',
   [
@@ -28,8 +65,74 @@ router.post(
   authLocal,
   signIn,
 )
+
+/**
+ * @openapi
+ * /auth/sign:
+ *   get:
+ *     tags:
+ *       - Auth
+ *     summary: 로그인 체크
+ *     description: 토큰 유효한지 체크용
+ *     security:
+ *       - AccessToken: []
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/successResponse'
+ *             example:
+ *               statusCode: 200
+ *               message: "로그인 상태"
+ */
 router.get('/sign', authJWT, signCheck)
+
+/**
+ * @openapi
+ * /auth/sign:
+ *   delete:
+ *     tags:
+ *       - Auth
+ *     summary: 로그아웃
+ *     security:
+ *       - AccessToken: []
+ *     responses:
+ *       200:
+ *         description: 로그아웃 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/successResponse'
+ *             example:
+ *               statusCode: 200
+ *               message: "로그아웃 성공"
+ */
 router.delete('/sign', authJWT, signOut)
+
+/**
+ * @openapi
+ * /auth/sign/new:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: 토큰 재발급
+ *     security:
+ *       - RefreshToken: []
+ *     responses:
+ *       201:
+ *         description: 토큰 재발급 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/signNewSuccessResponse'
+ *             example:
+ *               statusCode: 201
+ *               message: 토큰 재발급 성공
+ *               data:
+ *                  accessToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+ */
 router.post('/sign/new', authJWTRefresh, tokenRefresh)
 
 export default router
